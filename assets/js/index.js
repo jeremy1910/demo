efde486f374437627960f5e9543c69c5b809756a
jérémy <jeremy1910@gmail.com>
2019-09-15 22:51:06 +0200

    commit du 18/09/19 23:00

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+require('../css/index.css')
+
 const ratio = .2;
 const options = {
     root: null,
@@ -19,4 +21,19 @@ let observer = new IntersectionObserver(handleIntersect, options);
 document.querySelectorAll('.revealX').forEach(function (r) {
     observer.observe(r);
 });
+
+
+$('.js-index-card').hover(function (e) {
+
+    console.log();
+
+    $(this).prev().children().first().removeClass('offset-img');
+    $(this).prev().children().first().addClass('offset-img-none');
+}, function (e) {
+    $(this).prev().children().first().removeClass('offset-img-none');
+    $(this).prev().children().first().addClass('offset-img');
+
+});
+
+
 
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +15,14 @@ class IndexController extends AbstractController
      * @var ArticleRepository
      */
     private $repository;
+    private $categoryRepository;
+    const NB_COL_BY_SLIDE = 3;
 
-    public function __construct(ArticleRepository $repository)
+    public function __construct(ArticleRepository $repository, CategoryRepository $categoryRepository)
     {
 
         $this->repository = $repository;
+        $this->categoryRepository = $categoryRepository;
     }
 
     /**
@@ -28,9 +32,14 @@ class IndexController extends AbstractController
     {
         $articles = $this->repository->find5lastArticles();
 
+        $nbCategory = $this->categoryRepository->getNumberOfCategory();
+        $categories = $this->categoryRepository->findByCondition(null, 3, 0);
+
+
 
         return $this->render('index/index.html.twig', [
             'articles' => $articles,
+            'categories' => $categories,
         ]);
     }
 }
